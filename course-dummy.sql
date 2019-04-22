36a4bfdd17521b0735ac66dec0d2f772f10e60f3
Beryce Garcia <bgarcia@wellesley.edu>
2019-04-22 00:05:20 +0000

    scripts with dummy data

---
+++ course-dummy.sql
@@ -0,0 +1,5 @@
+use c9;
+
+insert into courses(name,semester, avg_rating, avg_hours) values ("ECON101", "F18", 0, 0);
+insert into courses(name,semester, avg_rating, avg_hours) values ("CS111", "S17", 0, 0);
+insert into courses(name,semester, avg_rating, avg_hours) values ("ENG115", "F16", 0, 0);
---
+++ courses-dummy-data.sql
@@ -0,0 +1,4 @@
+
+use c9;
+
+load data local infile 'course-data.csv' into table courses fields terminated by '\t' lines terminated by '\n';
